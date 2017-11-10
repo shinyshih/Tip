7ba2af63ad7530ad1a7e1279a443003c8dc96f6f
施馨檸 <shiny761118@gmail.com>
2017-11-10 10:09:07 +0800

    tip app.

--- Tip/ViewController.swift
+++ Tip/ViewController.swift
@@ -10,6 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var moneyTextField: UITextField!
+    @IBOutlet weak var percentTextField: UITextField!
+    
+    @IBOutlet weak var showLabel: UILabel!
+    
+    @IBAction func calculateButtonPressed(_ sender: Any) {
+        view.endEditing(true)
+        if moneyTextField.text == "" || percentTextField.text == "" {
+            showLabel.text = "空格填完才幫你算！"
+        } else {
+            let tip = Double(moneyTextField.text!)! * Double(percentTextField.text!)! / 100
+            showLabel.text = "小費：＄\(tip)"
+        }
+    }
+    
+    @IBAction func endEditing(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func clearButtonPressed(_ sender: Any) {
+        moneyTextField.text = ""
+        percentTextField.text = ""
+        showLabel.text = ""
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
